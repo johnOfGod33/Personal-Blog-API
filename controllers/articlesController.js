efc3519df9bfdcd293bfe008328b55bd6f708732
jeandedieu <jeandedieusessou@gmail.com>
2024-07-13 11:15:56 +0000

    IMPLEMENT ARTICLES ENDPONTS
    implement :
    - update comment (update content or published state or imageurl)
    - add comment

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -1,3 +1,4 @@
+const Article = require("../models/Article");
 const Articles = require("../models/Article");
 
 exports.createArticle = (req, res) => {
@@ -42,9 +43,21 @@ exports.getOneArticle = (req, res) => {
     .catch((err) => res.status(500).json(err));
 };
 
-exports.updateArticle = (req, res) => {};
+exports.updateArticle = (req, res) => {
+  const articleId = req.params.id;
 
-exports.addComment = (req, res) => {};
+  Article.updateOne({ _id: articleId }, { $set: req.body })
+    .then((value) => res.status(200).json(value))
+    .catch((err) => res.status(500).json(err));
+};
+
+exports.addComment = (req, res) => {
+  const articleId = req.params.id;
+
+  Article.updateOne({ _id: articleId }, { $push: { Comments: req.body } })
+    .then((value) => res.status(200).json(value))
+    .catch((err) => res.status(500).json(err));
+};
 
 exports.deleteArticle = (req, res) => {
   const articleId = req.params.id;
