5de5a4639246031c5bf2dd19b449aedbd4a9d64a
jeandedieu <jeandedieusessou@gmail.com>
2024-07-16 10:34:45 +0000

    add cors option

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const connectionDb = require("./middlewares/dbConfig");
 const usersRoute = require("./routes/usersRoute");
 const articlesRoute = require("./routes/articlesRoute");
@@ -13,6 +15,7 @@ connectionDb((err) => {
   }
 });
 
+app.use(cors({ origin: process.env.ORIGIN }));
 app.use(express.json());
 app.use("/users", usersRoute);
 app.use("/articles", articlesRoute);
